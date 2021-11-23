dc6d676a8c062a8462f0a0c70dfd2923e963402f
rustydb <doomslayer@hpe.com>
2021-11-23 12:04:03 -0600

    MTL-1560 Support Single Disk RAIDs
    This fixes a bug that was discovered while using a single disk RAID (for
    use in virtual or lab testing). This fixes an early exit, if multiple
    candidates are found and the first one was insufficient the disk scanner
    would exit early. This change ensures that we're always checking all
    possible candidates.

--- 93metalluksetcd/metal-luksetcd-disk.sh
+++ 93metalluksetcd/metal-luksetcd-disk.sh
@@ -6,19 +6,20 @@ ls /dev/sd* > /dev/null 2>&1 || exit 1
 
 # SUBBROUTINES
 type make_etcd > /dev/null 2>&1 || . /lib/metal-luksetcd-lib.sh
+type metal_resolve_disk > /dev/null 2>&1 || . /lib/metal-lib.sh
 
 # DISKS or RETRY
 # Ignore whatever was selected for the overlay by starting +1 from that index.
-disk_index=$((${metal_disks:-2} + 1))
-etcd="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '(sata|nvme|sas)' | sort -h | awk '{print $1 " " $2}' | tail -n +${disk_index} | tr '\n' ' ')"
+disk_offset=$((${metal_disks:-2} + 1))
+etcd="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '(sata|nvme|sas)' | sort -h | awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ')"
 [ -z "${etcd}" ] && exit 1
-etcd_size="$(echo $etcd | awk '{print $1}')"
+
+# Find the right disk.
 # 524288000000 is 0.5 TiB; required for this disk.
 # exit 0 ; this module does not need to run on this node unless it meets the requirements.
-[ "${etcd_size}" -gt 524288000000 ] && exit 0
+export etcd_disk=$(metal_resolve_disk "$etcd" 524288000000) || exit 0
 
-# DISKS
-export etcd_disk="$(echo $etcd | awk '{print $2}')"
+# Process the disk.
 if [ "${metal_nowipe:-0}" = 0 ]; then
     [ ! -f /tmp/metaletcddisk.done ] && make_etcd "$etcd_disk"
 fi
