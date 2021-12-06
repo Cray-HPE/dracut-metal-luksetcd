448d66f1b942f79161f5f50e3b277b9c077d752a
Russell Bunch <doomslayer@hpe.com>
2021-12-06 08:15:30 -0600

    MTL-1561 adjust sorting to cease grabbing GCP partitionn (#19)
    This reverts commit e0b3a383059129a5e7105fd6f11902a1089c91ea with fixes
    that omit NVME partitions on Google.

--- 93metalluksetcd/metal-luksetcd-disk.sh
+++ 93metalluksetcd/metal-luksetcd-disk.sh
@@ -10,8 +10,9 @@ type metal_resolve_disk > /dev/null 2>&1 || . /lib/metal-lib.sh
 
 # DISKS or RETRY
 # Ignore whatever was selected for the overlay by starting +1 from that index.
+# MAINTAINER NOTE: Regardless of gcp mode, this will ignore any NVME partition incase they stick around after wiping.
 disk_offset=$((${metal_disks:-2} + 1))
-etcd="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '('"$metal_transports"')' | sort -u | awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ' | sed 's/ *$//')"
+etcd="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '('"$metal_transports"')' | sort -h | grep -vE 'p[0-9]+$' |  awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ' | sed 's/ *$//')"
 [ -z "${etcd}" ] && exit 1
 
 # Find the right disk.
