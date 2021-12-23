69a9bc017acd66e6124598e3b0af1b08f721f64c
rustydb <doomslayer@hpe.com>
2021-12-23 14:01:09 -0600

    MTL-1407 Honor rd.luks=0 Throughout

--- 93metalluksetcd/parse-metal-luksetcd.sh
+++ 93metalluksetcd/parse-metal-luksetcd.sh
@@ -12,7 +12,9 @@ getargbool 0 metal.debug -d -y metal_debug && metal_debug=1
 metal_size_etcdk8s=$(getargnum 32 10 64 metal.disk.etcdk8s.size)
 
 getargbool 0 metal.no-wipe -d -y metal_nowipe && metal_nowipe=1 || metal_nowipe=0
+getargbool 0 rd.luks -d -n rd_NO_LUKS && metal_noluks=1 || metal_noluks=0
 
+export metal_noluks 
 export metal_debug
 export metal_size_etcdk8s
 export metal_nowipe
