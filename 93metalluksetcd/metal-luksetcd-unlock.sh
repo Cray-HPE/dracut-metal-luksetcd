843b0bc37ae522e6ebc5dbafad4b96a714093957
Russell Bunch <doomslayer@hpe.com>
2022-05-23 17:51:39 -0500

    MTL-1288 Fix Race Condition and Debug Mode
    This fixes two race conditions.
    - Fix the race condition with the pave function. Do not create
    anything until after the pave function has ran to completion.
    - Fix the race condition with dmsquash-live, add wait_for_dev
    to prevent pivoting to root before luksetcd finishes.
    
    This also fixes debug. metal.debug was not honoring metal.debug=1 in some
    cases. Also add metal.debug to every possible file except for
    module-setup.sh.
    
    This change removes the need to key off metal.no-wipe. Instead if the
    etcd disk is found this module quits, otherwise if luks is enabled
    and a deployment server is set this module must find a disk.
    metal.no-wipe is just a safeguard to ensure wipe functions are not
    invoked when it's enabled. This makes behavior consistent with
    dracut-metal-mdsquash.
    
    Leverage the new disks_exist method to wait for /dev/sd or /dev/nvme
    devices. This ensures that pure NVME systems can deploy, otherwise only
    SD systems and mixed (SD + NVME) are compatible.
    
    Remove old shim for the metal-lib function since it exists in the
    right place now.
    
    Add/update License
    
    Remove hardcodes for the keystore path.

--- 93metalluksetcd/metal-luksetcd-unlock.sh
+++ 93metalluksetcd/metal-luksetcd-unlock.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 # metal-luksetcd-unlock.sh
 if [ $metal_noluks = 0 ]; then 
-    echo >2 'skipping unlocking of LUKS devices (rd.luks=0 was set on the cmdline)'
+    echo >&2 'skipping unlocking of LUKS devices (rd.luks=0 was set on the cmdline)'
     exit 0
 fi
-
-type unlock > /dev/null 2>&1 || . /lib/metal-luksetcd-lib.sh
+etcdk8s_scheme=${metal_etcdk8s%=*}
+etcdk8s_authority=${metal_etcdk8s#*=}
 
 # Only run on reboots, when we don't create this file from the creation method.
-[ ! -f /tmp/metaletcddisk.done ] && unlock "$(basename $(blkid -L ${metal_etcdlvm##*=}))"
+if ! blkid -s UUID -o value "/dev/disk/by-${etcdk8s_scheme,,}/${etcdk8s_authority^^}" >/dev/null; then
+    etcd_disk=$(blkid -L ${metal_etcdlvm##*=})
+    etcd_key_file='etcd.key'
+    etcd_keystore="$metal_keystore/${etcd_key_file}"
+    if [ ! -f "$etcd_keystore" ]; then
+        echo >&2 "Missing etcd key at $etcd_keystore"
+        exit 1
+    fi
+    cryptsetup --key-file "${etcd_keystore}" \
+            --verbose \
+            --batch-mode \
+            --allow-discards \
+            --type=luks2 \
+            --pbkdf=argon2id \
+            luksOpen "${etcd_disk}" "${ETCDLVM:-ETCDLVM}"
+fi
--- 93metalluksetcd/module-setup.sh
+++ 93metalluksetcd/module-setup.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# module-setup.sh
 
 # called by dracut
 check() {
@@ -29,9 +53,9 @@ install() {
     # These copy our meta files into areas the pivoted rootfs can read from - they must run before
     # the root is chrooted.
     inst_hook pre-mount 11 "$moddir/metal-update-keystore.sh"
-
-    # Unlock etcd; kernel params may not be enough without disabling systemd.
-    inst_hook pre-mount 12 "$moddir/metal-luksetcd-unlock.sh"
+    
+    # Unlock the device before mounting the rootfs
+    inst_hook pre-mount 20 "$moddir/metal-luksetcd-unlock.sh"
 
     # We depend on information being gathered from the initqueue, if it fails or has nothing then
     # we should not run.
