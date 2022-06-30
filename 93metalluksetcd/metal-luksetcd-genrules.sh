4e3a38d9c8f76701f5ceb4f691028ac214727c7a
Russell Bunch <doomslayer@hpe.com>
2022-06-30 10:51:24 -0500

    Ensure kdump never runs (CASMTRIAGE-3591)
    Tidy up the README.

--- 93metalluksetcd/metal-luksetcd-genrules.sh
+++ 93metalluksetcd/metal-luksetcd-genrules.sh
@@ -27,6 +27,14 @@
 
 command -v getargbool > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
+root=$(getarg root)
+case $root in 
+    kdump)
+        echo 'Not doing anything for kdump'
+        exit 0
+        ;;
+esac
+
 # Only run when luks is enabled and a deployment server is present.
 if getargbool 0 rd.luks -d -n rd_NO_LUKS; then
     if [ -n "${metal_server:-}" ]; then 
