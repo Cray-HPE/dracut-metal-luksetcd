6d0a3b76ba2236617b2388630079c6a5d6df49ae
Russell Bunch <doomslayer@hpe.com>
2024-09-27 13:46:21 -0500

    Fetch `METAL_SERVER` for ourselves
    Just for the sake of safety, we should fetch this variable for ourselves instead of relying on the timing of dracut. Yes, dracut-metal-mdsquash should run before us, but what if it didn't? Also, what if someone ran this ad-hoc?

--- 93metalluksetcd/metal-luksetcd-genrules.sh
+++ 93metalluksetcd/metal-luksetcd-genrules.sh
@@ -38,7 +38,8 @@ command -v getargbool > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 # Only run when luks is enabled and a deployment server is present.
 if getargbool 0 rd.luks -d -n rd_NO_LUKS; then
-    if [ -n "${metal_server:-}" ]; then 
+    metal_server=$(getarg metal.server=)
+    if [ -n "${metal_server:-}" ]; then
         wait_for_dev -n /dev/metal-luks
         /sbin/initqueue --settled --onetime --unique /sbin/metal-luksetcd-disk
     fi
